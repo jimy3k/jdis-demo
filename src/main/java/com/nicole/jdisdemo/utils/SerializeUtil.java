b1f02485efe8ba106d8bc8e2878be9480014c3b8
jimy3k <jimy3k@126.com>
2022-08-18 22:16:57 +0800

    Redis存储，对象序列化操作

---
+++ src/main/java/com/nicole/jdisdemo/utils/SerializeUtil.java
@@ -0,0 +1,44 @@
+package com.nicole.jdisdemo.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializeUtil {
+    /**
+     * 将JAVA 对象转换为byte数组 序列化过程
+     **/
+    public static byte[] serialize(Object object) {
+        ObjectOutputStream oos = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            //序列化
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            byte[] bytes = baos.toByteArray();
+            return bytes;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将byte数组转换为JAVA 对象 反序列化过程
+     **/
+    public static Object unserialize(byte[] bytes) {
+        if (bytes == null) return null;
+        ByteArrayInputStream bais =null;
+        try {
+            //反序列化
+            bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/test/java/com/nicole/jdisdemo/JdisDemoApplicationTests.java
+++ src/test/java/com/nicole/jdisdemo/JdisDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.nicole.jdisdemo;
 
+import com.nicole.jdisdemo.pojo.User;
+import com.nicole.jdisdemo.utils.SerializeUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Transaction;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)   //解决不能 @Autowired 自动注入的问题
@@ -139,15 +145,29 @@ public class JdisDemoApplicationTests {
 
         System.out.println("测试Redis服务器心跳中..... " + pong);
 
-        //设置键，值
+        //设置键，值；单条
         jedis.set("name", "jimy3k");
         jedis.set("age", "33");
 
-        //取值
+        //取值；单条
         String name = jedis.get("name");
         int age = Integer.parseInt(jedis.get("age"));
         System.out.println("姓名：" + name);
         System.out.println("年龄：" + age);
+
+        //设置键，值；多条
+        jedis.mset("name", "jimy3k","age","30","sex","男");
+
+        //删值；
+        jedis.del("sex");
+
+        //取值；多条
+        List<String> list = jedis.mget("name", "age","sex");
+        System.out.println("多条取值：");
+        for(int j= 0; j < list.size(); j++){
+            System.out.println(list.get(j));
+        }
+
 
         //设置List 键，值
         Long len = jedis.llen("userlist");
@@ -155,8 +175,89 @@ public class JdisDemoApplicationTests {
             jedis.lpush("userlist", "jimy", "jimy3k", "zhangsan", "lisi", "wangwu");
         }
 
-        //取值
+        //List 取值
         List<String> userlist = jedis.lrange("userlist", 0, -1);
         System.out.println("用户列表：" + userlist);
+
+        //设置Hash 键，值
+        Map<String,String> map = new HashMap<>();
+        map.put("name","jimy3k");
+        map.put("age","32");
+        map.put("addr","深圳");
+        map.put("sex","男");
+        map.put("score","98");
+        jedis.hmset("user3",map);
+
+        //Hash 取值
+        userlist = jedis.hmget("user3", "name","age","addr","sex","score");
+        System.out.println("user3：" + userlist);
+    }
+    /**
+     * 使用Jedis配置类 键值失效时间单元测试
+     **/
+    @Test
+    public void testExpired() {
+        //设置键值时，设置失效时间
+        jedis.setex("code",600,"验证码");
+
+        try {
+            TimeUnit.SECONDS.sleep(10);   //让程序等10秒钟
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //查询键值失效时间
+        long expiredTime = jedis.ttl("code");
+        System.out.println(expiredTime);
+
+        //设置已存在键值的失效时间
+        jedis.expire("code",60);
+
+        try {
+            TimeUnit.SECONDS.sleep(10);   //让程序等10秒钟
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //查询键值失效时间
+        expiredTime = jedis.ttl("code");
+        System.out.println(expiredTime);
+    }
+
+    /**
+     * 使用Jedis配置类 操作java对象
+     **/
+    @Test
+    public void testPojo() {
+        jedis.select(1);
+
+        User user = new User();
+        user.setId(2);
+        user.setUsername("张三");
+        user.setPassword("123456");
+        byte[] byteUser = SerializeUtil.serialize(user);
+
+        //设值
+        jedis.set(SerializeUtil.serialize("user"),byteUser);
+
+        //取值
+        byte[] user1 = jedis.get(SerializeUtil.serialize("user"));
+        User user2 = (User) SerializeUtil.unserialize(user1);
+        System.out.println(user2);
+    }
+
+    /**
+     * 使用Jedis配置类 使用事务
+     **/
+    @Test
+    public void testTx() {
+        Transaction tx = jedis.multi();
+        tx.set("code","1235rt3");
+
+        //执行事务
+        tx.exec();
+
+        //回滚事务
+        //tx.discard();
     }
 }
