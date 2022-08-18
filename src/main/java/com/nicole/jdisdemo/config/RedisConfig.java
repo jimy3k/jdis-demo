4c949524b1a5d92a1f0cc27851106d7c4f14d5c2
jimy3k <jimy3k@126.com>
2022-08-18 18:05:12 +0800

    增加JedisPool 连接池

---
+++ src/main/java/com/nicole/jdisdemo/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.nicole.jdisdemo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ *
+ * Redis 配置类
+ *
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private Integer port;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.timeout}")
+    private String timeout;
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private Integer maxActive;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private String maxWait;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private Integer maxIdle;
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private Integer minIdle;
+
+    @bean
+    public JedisPool jedisPool(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(maxActive);
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMinIdle(minIdle);
+        jedisPoolConfig.setMaxWaitMillis(Long.parseLong(maxWait.substring(0,maxWait.length()-2)));
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig,host,port,,password);
+        return jedisPool;
+    }
+
+}
